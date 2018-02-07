7ae0370a51597cd69f03ef370d4c640357ee1d0a
alejandrohuk <36174338+alejandrohuk@users.noreply.github.com>
2018-02-06 22:15:00 -0300

    6 feb if
    trabajo en clase if

---
+++ jsInstruccionIF (6).js
@@ -0,0 +1,22 @@
+function Mostrar()
+{
+//tomo la edad  
+
+	var edad;
+	edad= document.getElementById('edad').value;
+	if (edad<13 )
+	{
+		alert("es un niño");
+	}
+	else
+	{
+		if(edad >18) 
+		{
+			alert(es adulto);
+		}		
+		else
+
+	}
+
+// if else else
+}//FIN DE LA FUNCIÓN
