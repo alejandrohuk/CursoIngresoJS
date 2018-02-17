f1ef007ff4b664063511e084473a3e912ddda23f
alejandrohuk <36174338+alejandrohuk@users.noreply.github.com>
2018-02-17 12:09:22 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,18 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
-
+	var numeroRandom;
+	Math.floor((Math.random() * 10) + 1);
+	if(numeroRandom>8)
+	{
+		alert("exelente" + numeroRandom);
+	}else
+	{
+		if(numeroRandom>3)
+			{alert("aprobo" + numeroRandom);
+}
+		else
+			{ alert("la proxima sera");
+		}
+	}
 }//FIN DE LA FUNCIÓN
