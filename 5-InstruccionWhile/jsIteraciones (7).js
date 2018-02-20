b9a132d2a53d1f1882b996bc8b02b30c7dd4164f
alejandrohuk <36174338+alejandrohuk@users.noreply.github.com>
2018-02-20 19:38:44 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,9 +1,21 @@
 function Mostrar()
 {
 
-	var contador=0;
-	var acumulador=0;
-	var respuesta='si';
+		var contador=0;
+		var acumulador=0;
+		var respuesta='si';
+		var numero;
+
+		while(respuesta == "si")
+		{
+		
+			
+			
+			contador ++;
+			numero = prompt("ingrese un numero");
+			acumulador = parseInt(acumulador)  + parseInt(numero);
+			respuesta = prompt("desea continuar?");
+		}
 
 
 document.getElementById('suma').value=acumulador;
