4b43da1b9c9bea1408b18c878d9b03b229d08caa
alejandrohuk <36174338+alejandrohuk@users.noreply.github.com>
2018-02-20 21:34:59 -0300

    for

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,71 @@
 function Mostrar()
 {
+	//var contador;
+	//contador = 0;
+	/*var contador=0;
+	for(;;)
+	{
+		contador++
+		document.write("<br>" + contador);
+	
+		if(contador==10)
+		{
+			document.write("<br> + break" + contador);
+			break;
+		}
+		if(contador==5)
+		{
+			continue;
+		}
+		document.write( "<br> + continue" + contador);
 
-}
+
+	}*/
+		var contadorDePositivos=0;
+		var contadorDeNegativos=0;
+		var contadorDeCeros=0;
+		var contadorDePar=0;
+		var contadorDeimPar=0;
+		var contador50=0;
+
+
+		for(contador=-100; contador<100; contador++)
+		{
+			if(contador>0)
+			{
+				contadorDePositivos++;
+			}
+			else{
+				if(contador<0)
+					{
+						contadorDeNegativos++;
+
+					}else
+					{contadorDeCeros++;
+					}
+			}
+			if(contador%2==0 && contadorDeCeros!=0)
+			{
+				contadorDePar++;
+			}else
+			{
+				if(contadorDeCeros!=0)
+				{
+					contadorDeimPar++;
+				}
+			}
+			switch(contador)
+			{
+				case 50:
+					contador50 ++;
+					break;
+			}
+		}
+		document.write("positivos: "+contadorDePositivos)
+		document.write("<br>negativos: "+contadorDeNegativos)
+		document.write("<br>ceros: "+contadorDeCeros)
+		document.write("<br>impares: "+ contadorDeimPar)
+		document.write("<br>contadorde50: "+ contador50)
+		
+
+}//es la de la funcion
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,68 @@
 function Mostrar()
 {
+	var numeroRandom;
+	var numero;
+    var contador =0;
+    var contador0=0;
+    var contador1=0;
+    var contador2=0;
+    var contador3=0;
+    var contador4=0;
+    var contador5=0;
+    var contador6=0;
+    var contador7=0;
+    var contador8=0;
+    var contador9=0;
 
+	for( numero=0; numero<100; numero++ )
+	{
+	numeroRandom=Math.floor(Math.random() * 10);
+	//document.write(numeroRandom);
+	switch(numeroRandom)
+	{
+		case 0:
+		contador0++;
+		break;
+		case 1:
+		contador1++;
+		break;
+		case 2:
+		contador2++;
+		break;
+		case 3:
+		contador3++;
+		break;
+		case 4:
+		contador4++;
+		break;
+		case 5:
+		contador5++;
+		break;
+		case 6:
+		contador6++;
+		break;
+		case 7:
+		contador7++;
+		break;
+		case 8:
+		contador8++;
+		break;
+		case 9:
+		contador9++;
+		break;
+	}
+
+	}
+
+	document.write("<br>" + contador0)
+	document.write("<br>" + contador1)
+	document.write("<br>" + contador2)
+	document.write("<br>" + contador3)
+	document.write("<br>" + contador4)
+	document.write("<br>" + contador5)
+	document.write("<br>" + contador6)
+	document.write("<br>" + contador7)
+	document.write("<br>" + contador8)
+	document.write("<br>" + contador9)
 
 }
