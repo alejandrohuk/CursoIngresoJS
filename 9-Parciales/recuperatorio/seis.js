de4805d3ec0e69245b94014c52d5db410eb1d2ee
alejandrohuk <36174338+alejandrohuk@users.noreply.github.com>
2018-02-26 22:19:39 -0300

    Add files via upload

--- 9-Parciales/recuperatorio/seis.js
+++ 9-Parciales/recuperatorio/seis.js
@@ -1,4 +1,23 @@
 function Mostrar()
 {
+	var peso;
+	var contador=0;
+	var acumuladorDePeso=0;
+
+		while(contador<3)
+		{
+			contador++
+			peso= prompt("ingrese el peso del contenedor en kilos");
+			peso
+			while(peso <=0 || isNaN(peso))
+			{
+				peso= prompt("ingrese el peso del contenedor en kilos");
+			}
+			acumuladorDePeso=acumuladorDePeso + peso; 
+		}
+
+		if()
+
+
 
 }
